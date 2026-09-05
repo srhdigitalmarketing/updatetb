fcca72117e2be1ea666eec871e2f62f6724e72da
srhdigitalmarketing <srhdigitalmarketing@users.noreply.github.com>
2026-09-06 05:04:10 +0700

    Improve nested navigation readability

--- app/Views/admin/__layout/header.php
+++ app/Views/admin/__layout/header.php
@@ -30,7 +30,7 @@
 
     <!-- Custom Theme Style -->
     <link href="<?= site_url('/admin-assets/css/custom.min.css?v=1.2') ?>" rel="stylesheet">
-    <link href="<?= site_url('/admin-assets/css/admin-polish.css?v=20260906-5') ?>" rel="stylesheet">
+    <link href="<?= site_url('/admin-assets/css/admin-polish.css?v=20260906-6') ?>" rel="stylesheet">
 </head>
 
 <body class="nav-md">
